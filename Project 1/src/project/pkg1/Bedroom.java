9447d9b8b0041b11353a74ba4153fcb8ddfbe68c
breadwards96 - School <breadwards96@gmail.com>
2018-02-14 12:56:09 -0800

    New Class

--- Project 1/src/project/pkg1/Bedroom.java
+++ Project 1/src/project/pkg1/Bedroom.java
@@ -9,6 +9,13 @@ package project.pkg1;
  *
  * @author cs111005_13
  */
-public class Bedroom {
+public class Bedroom extends Room {
+
+    public Bedroom(String roomName, String objects, int roomNum, String listOfExits) {
+        super(roomName, objects, roomNum, listOfExits);
+    }
     
+    public void SleeponHay(){
+        
+    }
 }
