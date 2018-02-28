ec0b39fab4d6d745061d94fdfa5db2bf43365e8d
BrandonMac <breadwards96@gmail.com>
2018-02-28 11:57:44 -0800

    Project Final

--- Project2/src/project2/BinaryRecursiveSearch.java
+++ Project2/src/project2/BinaryRecursiveSearch.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project2;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
+ * @author Brandon Edwards
+ * @since 2/26/18
  *
- * @author brandonedwards
+ *
+ */
+/**
+ * honorCode I pledge that this submission is solely my work, and that I have
+ * neither given to nor received help from anyone other than the instructor or
+ * TAs.
+ *
  */
 public class BinaryRecursiveSearch {
-    
+
+    public BinaryRecursiveSearch() {
+    }
+
+    //search method
+    public long search(RandomAccessFile file, long start, long end, Short index)
+            throws IOException {
+
+        //finding the middle
+        long middle = (start + end) / 2;
+
+//        System.out.println("length: " + file.length());
+//        System.out.println("pos: " + middle);
+        file.seek((middle * 2));
+
+        //for position processing
+        short number = file.readShort();
+
+        //case for number not found
+        if (middle >= file.length() || middle < start) {
+//            System.out.println("File position: " + middle
+//            + "\nFile length: " + file.length());
+//            System.out.println("Number: " + number);
+            return -1;
+        }
+
+        //base case for number found
+        if (number == index) {
+//            System.out.println("Number: " + number);
+            return middle;
+        }
+        
+        //recursion for too high or too low
+        if (number < index) {
+//            System.out.println("Number: " + number);
+            return search(file, middle + 1, end, index);
+        } else {
+//            System.out.println("Number: " + number);
+            return search(file, start, middle - 1, index);
+        }
+    }
 }
--- Project2/src/project2/RandomAccessBinaryFileRecursiveBinarySearch.java
+++ Project2/src/project2/RandomAccessBinaryFileRecursiveBinarySearch.java
@@ -1,21 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
+ * @author Brandon Edwards
+ * @since 2/26/18
  *
- * @author brandonedwards
+ *
+ */
+/**
+ * honorCode I pledge that this submission is solely my work, and that I have
+ * neither given to nor received help from anyone other than the instructor or
+ * TAs.
+ *
  */
 public class RandomAccessBinaryFileRecursiveBinarySearch {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        //fields
+        boolean prompt = true;
+        int cycle = 0;
+        short index = 0;
+        Scanner keyboard = new Scanner(System.in);
+        File binFileShort = new File("BinFileShort");
+        RandomAccessFile randoFile = new RandomAccessFile(binFileShort, "r");
+        BinaryRecursiveSearch recurSearcher = new BinaryRecursiveSearch();
+
+        //loop to prompt user 10 times
+        while (prompt) {
+
+            if (cycle > 10) {
+                prompt = false;
+            }
+
+            System.out.println("The File is " + randoFile.length()
+                    + " Bytes long");
+
+            System.out.println("Please enter a number to look for: ");
+            
+            //Error Handling
+            try{
+            index = keyboard.nextShort();
+            }catch(InputMismatchException e){
+                System.out.println("Sorry that's not a valid number. Closing.");
+                break;
+            }
+            
+            //search function
+            long result = recurSearcher.search(randoFile,
+                    0, randoFile.length() - 1, index);
+
+            //result processing and subsequent output
+            if (result == -1) {
+                System.out.println("The number " + index
+                        + " could not be found in the file");
+            } else {
+                System.out.println("The number " + index
+                        + " was found at file position "
+                        + result);
+            }
+            cycle++;
+        }
+
     }
-    
+
 }
